d0e1bfef82b0514ecbb7a3bcaa8cf3e12fcb8a7c
Sean O'Sullivan <sosulliv@Seans-MacBook-Pro.local>
2018-02-04 18:34:39 -0500

    controller and server split

--- server.js
+++ server.js
@@ -4,29 +4,34 @@ import express from 'express';
 import open from 'open';
 import bodyParser from 'body-parser';
 import exphbs from 'express-handlebars';
+import router from './controllers/controller.js';
+
 
 
 
 /* eslint-disable no-console*/
 
-const port = 3006;
+const port = 3007;
 const url = 'http://localhost:';
 const app = express();
 
+//express config
+app.use(express.static(process.cwd() + '/public'));
+
+//body parser config
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+
+//handlebars config
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
-
-app.get('/', function (req, res) {
-	res.render('index');
-});
-
+//router
+app.use('/', router);
 
 
 app.get('/users', function (req, res) {
@@ -38,6 +43,8 @@ app.get('/users', function (req, res) {
 	]);
 });
 
+
+// Open Server
 app.listen(port, function (err) {
 	if (err) { console.log(err); }
 	else {
